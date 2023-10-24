c267439700585d1109272ad4c1c81fcaca890a34
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-24 21:58:01 +0000

    Update exceptions.ts
    Add error which is an optional field

--- system/src/middleware/exceptions/exceptions.ts
+++ system/src/middleware/exceptions/exceptions.ts
@@ -1,10 +1,13 @@
 class HttpException extends Error {
   status: number;
   message: string;
-  constructor(status: number, message: string) {
+  error?: object;
+
+  constructor(status: number, message: string, error?: object) {
     super(message);
     this.status = status;
     this.message = message;
+    this.error = error;
   }
 }
 
