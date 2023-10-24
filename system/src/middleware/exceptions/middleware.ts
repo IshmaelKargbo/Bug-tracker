e07254b8c93d008563ba9ddf17833e5ad78f6752
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-24 21:58:34 +0000

    Update middleware.ts
    Add error which is an optional field

--- system/src/middleware/exceptions/middleware.ts
+++ system/src/middleware/exceptions/middleware.ts
@@ -1,14 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 import HttpException from './exceptions';
  
-function ErrorMiddleware(error: HttpException, request: Request, response: Response, next: NextFunction) {
-    const status = error.status || 500;
-    const message = error.message || 'Something went wrong';
+function ErrorMiddleware(err: HttpException, request: Request, response: Response, next: NextFunction) {
+    const status = err.status || 500;
+    const message = err.message || 'Something went wrong';
+    const error = err.error;
+    
     response
       .status(status)
       .send({
         status,
         message,
+        error
       })
   }
    
