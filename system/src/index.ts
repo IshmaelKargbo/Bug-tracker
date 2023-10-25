29548c73f67b3c33951aa833dacf2e8b675e4c13
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-25 20:36:14 +0000

    remove system

--- system/src/index.ts
+++
@@ -1,9 +0,0 @@
-import App from "./app";
-import { PORT } from "./config/env";
-import UserController from "./modules/user/controller";
-
-const app = new App([
-  new UserController()
-], PORT);
-
-app.listen();
