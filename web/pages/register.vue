e6988a548221f76d21c18d3268af69b9bb278bef
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-25 14:39:38 +0000

    Update register.vue
    fix theme

--- web/pages/register.vue
+++ web/pages/register.vue
@@ -1,51 +1,47 @@
 <template>
-  <div class="bg-gray-50 flex min-h-screen items-center justify-center">
+  <div
+    class="bg-gray-50 dark:bg-gray-900 flex min-h-screen items-center dark:text-white justify-center"
+  >
     <div class="w-2/6 flex flex-col h-screen">
       <div class="flex-1 h-full items-center flex">
         <div class="w-full">
           <div class="mb-6">
-          <p class="text-2xl mb-0.5 font-inter">Get Started</p>
-          <p class="text-sm text-gray-400">Create an account</p>
-        </div>
-        <div>
-          <button class="border w-full py-2.5 rounded-md bg-white block">
-            Continue with Github
-          </button>
-          <button class="border w-full py-2.5 mt-2 rounded-md bg-white block">
-            Continue with Google
-          </button>
-          <div class="flex items-center my-5">
-            <hr class="flex-1" />
-            <p class="px-2.5">or</p>
-            <hr class="flex-1" />
+            <p class="text-2xl mb-0.5 font-inter">Get Started</p>
+            <p class="text-sm text-gray-400">Create an account</p>
           </div>
-          <form @submit.prevent>
-            <div class="mb-5">
-              <label for="email" class="text-sm text-gray-400">Email</label>
-              <div
-                class="bg-white border rounded-md overflow-hidden p-3 mt-1.5 text-sm"
-              >
-                <input
+          <div>
+            <Button class="mb-5">Continue with Github</Button>
+            <Button> Continue with Google</Button>
+            <div class="flex items-center mt-6 mb-5">
+              <hr class="flex-1 dark:border-gray-700" />
+              <p class="px-2.5 dark:text-gray-400 text-xs">OR</p>
+              <hr class="flex-1 dark:border-gray-700" />
+            </div>
+            <form @submit.prevent>
+              <div class="mb-5">
+                <Input
                   id="email"
-                  type="email"
-                  placeholder="you@example.com"
-                  class="w-full bg-transparent focus:outline-none"
+                  name="email"
+                  label="Email"
+                  placeholder="yes@example.com"
                 />
               </div>
+              <Button primary>Register</Button>
+            </form>
+            <div class="flex space-x-1 text-sm mt-8 justify-center">
+              <p class="text-gray-400">Have an account?</p>
+              <NuxtLink to="/login" class="underline text-emerald-600">Login Now</NuxtLink>
             </div>
-            <button class="bg-emerald-600 w-full rounded py-2.5 text-white">
-              Sign Up
-            </button>
-          </form>
-          <div class="flex space-x-1 text-sm mt-8 justify-center">
-            <p class="text-gray-500">Have an account?</p>
-            <NuxtLink to="/login" class="underline">Login Now</NuxtLink>
           </div>
-        </div>
         </div>
       </div>
       <div class="py-8">
-        <p class="text-xs text-center">By continuing, you agree to Bug Tracker <NuxtLink to="/login" class="underline">Terms of Service</NuxtLink> and <NuxtLink to="/login" class="underline">Privacy Policy</NuxtLink>, and to receive periodic emails with updates.</p>
+        <p class="text-xs text-center dark:text-gray-300">
+          By continuing, you agree to Bug Tracker
+          <NuxtLink to="/login" class="underline text-emerald-600">Terms of Service</NuxtLink>
+          and <NuxtLink to="/login" class="underline text-emerald-600">Privacy Policy</NuxtLink>,
+          and to receive periodic emails with updates.
+        </p>
       </div>
     </div>
   </div>
