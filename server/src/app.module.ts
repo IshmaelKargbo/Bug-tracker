061ec666404b4a687d1b73d4ff2cf6bfc61b917b
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-25 20:37:01 +0000

    Configure typeorm with postgres

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'admin',
+        password: 'admin',
+        database: 'bug_tracker',
+        entities: [],
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
