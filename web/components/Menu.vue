8f1478a584c79fd1920bf4c5d590103729a6b2da
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-26 08:08:36 +0000

    Update Menu.vue

--- web/components/Menu.vue
+++ web/components/Menu.vue
@@ -5,19 +5,19 @@
     </div>
     <ul class="pt-5">
       <li>
-        <NuxtLink to="/" class="hover:bg-emerald-600 space-x-2.5 rounded-md hover:text-gray-100 p-3 flex items-center">
+        <NuxtLink to="/" class="hover:bg-emerald-600 space-x-3 rounded-md hover:text-gray-100 p-3 flex items-center">
           <Icon name="akar-icons:dashboard" class="text-xl mb-1" />
           <span>Dashboard</span>
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/" class="hover:bg-emerald-600 space-x-2.5 rounded-md hover:text-gray-100 p-3 flex items-center">
+        <NuxtLink to="/" class="hover:bg-emerald-600 space-x-3 rounded-md hover:text-gray-100 p-3 flex items-center">
           <Icon name="lucide:projector" class="text-xl mb-1" />
           <span>Projects</span>
         </NuxtLink>
       </li>
       <li>
-        <NuxtLink to="/" class="hover:bg-emerald-600 space-x-2.5 rounded-md hover:text-gray-100 p-3 flex items-center">
+        <NuxtLink to="/" class="hover:bg-emerald-600 space-x-3 rounded-md hover:text-gray-100 p-3 flex items-center">
           <Icon name="mdi:sticker-text-outline" class="text-xl mb-1" />
           <span>Tickets</span>
         </NuxtLink>
