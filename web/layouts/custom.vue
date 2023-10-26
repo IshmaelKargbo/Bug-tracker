5ed9eaef005c3753644384b3be7a1d44575661c0
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-26 18:57:54 +0000

    Update custom.vue

--- web/layouts/custom.vue
+++ web/layouts/custom.vue
@@ -1,5 +1,7 @@
 <template>
-  <main class="font-poppins text-sm">
-    <slot />
-  </main>
+  <ClientOnly>
+    <main class="font-poppins text-sm dark:bg-gray-900 bg-slate-100">
+      <slot />
+    </main>
+  </ClientOnly>
 </template>
