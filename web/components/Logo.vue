6a6696c674255eea9d992a26f41cd1ebeb5561f9
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-26 03:34:12 +0000

    Create Logo.vue

---
+++ web/components/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLink :to="to" class="flex space-x-2 items-center">
+    <p class="font-caveat text-emerald-600 text-2xl">Bug nn</p>
+    <div>
+      <Icon name="fa6-solid:hammer" class="text-gray-700 text-2xl" />
+      <Icon name="clarity:bug-solid" class="text-red-600 text-xl" />
+    </div>
+  </NuxtLink>
+</template>ˇ
+
+<script setup lang="ts">
+const {to} = defineProps<{to: string}>();
+</script>
