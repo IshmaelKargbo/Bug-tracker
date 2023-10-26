d7aaf3fab2b332d36783c8b8fc67771bbd50ae66
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-26 21:27:11 +0000

    Create MenuItem.vue

---
+++ web/components/MenuItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <NuxtLink
+    :to="to"
+    active-class="bg-emerald-900 text-gray-100"
+    class="hover:bg-emerald-900 mb-2 dark:text-gray-400 space-x-3 rounded-md hover:text-gray-100 p-3 px-4 flex items-center"
+  >
+    <slot />
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+interface Props {
+    to: string;
+}
+
+const {to} = defineProps<Props>();
+</script>
+
+<style scoped>
+</style>
