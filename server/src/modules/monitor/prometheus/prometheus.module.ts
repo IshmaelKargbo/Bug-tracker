b11648afb5f5ee999f91415be44a7de02a186897
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-27 04:58:02 +0000

    Update prometheus.module.ts

--- server/src/modules/monitor/prometheus/prometheus.module.ts
+++ server/src/modules/monitor/prometheus/prometheus.module.ts
@@ -1,47 +1,8 @@
-import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from '../health/health.controller';
-import { HealthService } from '../health/health.service';
 import { Module } from '@nestjs/common';
+import { PrometheusService } from './prometheus.service';
 
 @Module({
-  imports: [TerminusModule],
-  controllers: [HealthController],
-  providers: [HealthService],
-  exports: [HealthService],
+  providers: [PrometheusService],
+  exports: [PrometheusService],
 })
-export class UserModule {
-  public registerMetrics(
-    name: string,
-    help: string,
-    labelNames: string[],
-    buckets: number[]
-  ): Histogram<string> {
-    if (this.registeredMetrics[name] === undefined) {
-      const histogram = new Histogram({ name, help, labelNames, buckets });
-      this.registry.registerMetric(histogram);
-      this.registeredMetrics[name] = histogram;
-    }
-    return this.registeredMetrics[name];
-  }
-
-  public registerGauge(name: string, help: string): Gauge<string> {
-    if (this.registeredGauges[name] === undefined) {
-      const gauge = (this.registeredGauges[name] = new Gauge({
-        name: this.servicePrefix + name,
-        help,
-      }));
-      this.registry.registerMetric(gauge);
-      this.registeredGauges[name] = gauge;
-    }
-    return this.registeredGauges[name];
-  }
-
-  public removeSingleMetric(name: string): void {
-    return this.registry.removeSingleMetric(name);
-  }
-
-  public clearMetrics(): void {
-    this.registry.resetMetrics();
-    return this.registry.clear();
-  }
-}
+export class PrometheusModule {}
