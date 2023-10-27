7b655334fd9061c2b5a51c9f0cb7eceb1469ac46
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-27 03:32:40 +0000

    setup an exception handler

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './pipes/exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(4000);
 }
 bootstrap();
