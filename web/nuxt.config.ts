8ab03501aed5e3955b10528be4805696b5a3ac5d
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-28 20:30:40 +0000

    Update nuxt.config.ts

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,14 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ui: {},
   googleFonts: {
     families: {
-      'Open Sans': true,
+      "Open Sans": true,
       Poppins: [300],
       Caveat: [700],
       Inter: [200, 500, 700],
     },
   },
-  modules: ["@nuxtjs/google-fonts", "@nuxt/ui", "nuxt-icon"],
+  modules: [
+    "@nuxtjs/google-fonts",
+    "@nuxt/ui",
+    "nuxt-icon",
+    "@sidebase/nuxt-auth"
+  ],
 });
