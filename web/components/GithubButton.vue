3668aa584e8f46d942783848a7e435bc17522f29
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-28 20:30:35 +0000

    Create GithubButton.vue

---
+++ web/components/GithubButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <UButton
+    @click="handleSignIn"
+    class="w-full flex justify-center -space-x-1 mb-2"
+    size="xl"
+    variant="outline"
+    color="gray"
+  >
+    <Icon name="line-md:github-loop" />
+    <span>Continue with Github</span>
+  </UButton>
+</template>
+
+<script setup lang="ts">
+const { status, data, signIn, signOut } = useAuth()
+
+const handleSignIn = async () => {
+  await signIn();
+};
+</script>
