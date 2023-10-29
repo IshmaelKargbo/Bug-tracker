d7be1cfd703f86c2cebf77909f23cf4b36d5faa2
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-29 08:36:16 +0000

    login with google on backend

---
+++ web/components/GoogleButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <UButton
+    @click="handleSignIn"
+    class="w-full flex justify-center -space-x-1 mb-2"
+    size="xl"
+    variant="outline"
+    color="gray"
+  >
+    <Icon name="fe:google" />
+    <span>Continue with Google</span>
+  </UButton>
+</template>
+
+<script setup lang="ts">
+const handleSignIn = async () => {
+  window.open("http://localhost:4000/auth/google", "_self");
+};
+</script>
