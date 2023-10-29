ed2cab1c6e85e5713de91a706dab467cc730b146
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-29 08:38:05 +0000

    Update auth.module.ts

--- server/src/modules/auth/auth.module.ts
+++ server/src/modules/auth/auth.module.ts
@@ -1,15 +1,40 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
-import { GithubStrategy } from './github.strategy';
+import { GithubStrategy } from './strategy/github';
 import { UserService } from '../user/user.service';
 import UserEntity from '../user/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { GoogleStrategy } from './google.strategy';
+import { GoogleStrategy } from './strategy/google';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtConfig } from 'src/config/configuration';
+import { JwtStrategy } from './strategy/jwt';
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const jConfig = config.get<JwtConfig>('jwt');
+
+        return {
+          secret: jConfig.secret,
+          signOptions: { expiresIn: '60s' },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [UserService, GithubStrategy, GoogleStrategy],
+  providers: [
+    UserService,
+    GithubStrategy,
+    GoogleStrategy,
+    JwtStrategy,
+    AuthService,
+  ],
   exports: [],
 })
 export class AuthModule {}
