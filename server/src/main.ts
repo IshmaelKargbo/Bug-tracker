c20cebe2609b59915e95c5d3f92f55961bc1f01e
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-29 08:37:56 +0000

    add session

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './pipes/exception';
 import { EmojiLogger } from './common/emoji-logger';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +12,21 @@ async function bootstrap() {
   });
 
   app.enableCors();
+
+  app.use(
+    session({
+      secret: 'McQfTjWmZq4t7w!z4u7x!A%D*F-JaNd',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 5,
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   app.useGlobalPipes(
     new ValidationPipe({
