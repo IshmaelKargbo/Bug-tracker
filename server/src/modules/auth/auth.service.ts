b918cfe7ac6796aa8f51c9b35a1c459e9d040516
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-29 08:37:39 +0000

    Create auth.service.ts

---
+++ server/src/modules/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from '../user/user.service';
+import { AuthDTO } from '../user/user.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private service: UserService, private jwt: JwtService) {}
+
+  async access(dto: AuthDTO): Promise<any> {
+    const user = await this.service.findOrCreate(dto);
+    const payload = { username: user.email, sub: user.id };
+    return this.jwt.sign(payload);
+  }
+}
