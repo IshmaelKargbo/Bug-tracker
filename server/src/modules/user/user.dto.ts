a916998ebca2de83aa08587d530a7d24a9da8bd2
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-29 08:36:48 +0000

    Update user.dto.ts

--- server/src/modules/user/user.dto.ts
+++ server/src/modules/user/user.dto.ts
@@ -1,14 +1,38 @@
 import { OmitType } from '@nestjs/mapped-types';
-import { IsEmail, IsUUID } from 'class-validator';
+import { IsEmail, IsString, IsUUID } from 'class-validator';
 
-class UserDto {
+class UserDTO {
   @IsEmail()
   email: string;
+
+  @IsString()
+  givenNames: string;
+
+  @IsString()
+  familyName: string;
+
+  @IsString()
+  public image: string;
+
+  @IsString()
+  public provider: string;
+
+  @IsString()
+  public password: string;
 
   @IsUUID()
   id: string;
 }
 
-export class CreateUserDto extends OmitType(UserDto, ['id'] as const) {}
+export class NewUserDTO extends OmitType(UserDTO, ['id', 'image'] as const) {}
 
-export class FindOneParams extends OmitType(UserDto, ['email'] as const) {}
+export class AuthDTO extends OmitType(UserDTO, ['id', 'password'] as const) {}
+
+export class FindOneParams extends OmitType(UserDTO, [
+  'email',
+  'familyName',
+  'givenNames',
+  'image',
+  'password',
+  'provider',
+] as const) {}
