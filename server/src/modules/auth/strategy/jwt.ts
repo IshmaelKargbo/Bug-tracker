ade6b023a712039d7ed2416db221effe7a9bdb53
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-30 20:10:16 +0000

    Update jwt.ts

--- server/src/modules/auth/strategy/jwt.ts
+++ server/src/modules/auth/strategy/jwt.ts
@@ -2,16 +2,16 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { JwtConfig } from 'src/config/configuration';
+import { AppConfig } from 'src/config/configuration';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly config: ConfigService) {
-    const jConfig = config.get<JwtConfig>('jwt');
+    const appConfig = config.get<AppConfig>('app');
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: jConfig.secret,
+      secretOrKey: appConfig.secret,
     });
   }
 
