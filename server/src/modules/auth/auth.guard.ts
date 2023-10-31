f0db7157301150017a63343c82cf0012bf5eb873
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-31 07:05:49 +0000

    Create auth.guard.ts
    To check for jwt token

---
+++ server/src/modules/auth/auth.guard.ts
@@ -0,0 +1,62 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { AppConfig } from 'src/config/configuration';
+import { UserService } from '../user/user.service';
+import { Payload } from 'src/common/interface';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from './public.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private readonly config: ConfigService,
+    private readonly service: UserService,
+    private readonly reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const aConfig = this.config.get<AppConfig>('app');
+
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload: Payload = await this.jwtService.verifyAsync(token, {
+        secret: aConfig.secret,
+      });
+
+      const user = await this.service.findOne(payload.sub);
+
+      request['user'] = user;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
