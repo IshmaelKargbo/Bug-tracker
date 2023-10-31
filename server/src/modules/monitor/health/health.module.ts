ccff45f212a8bcace9bc65db13e030f970c94d42
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-31 16:54:27 +0000

    change filename

--- server/src/modules/monitor/health/health.module.ts
+++
@@ -1,9 +0,0 @@
-import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from './health.controller';
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [TerminusModule],
-  controllers: [HealthController],
-})
-export class HealthModule {}
--- server/src/modules/user/module/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { TypeOrmModule } from '@nestjs/typeorm';
-import UserEntity from '../user.entity';
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  exports: [TypeOrmModule],
-})
-export class UserModule {}
