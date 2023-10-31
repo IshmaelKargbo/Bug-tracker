4d7b535d638982931335355217e1755b273bab1e
Ishmael Kargbo <ishmael.kargbo@speedykom.de>
2023-10-31 07:07:44 +0000

    Update http.module.ts
    add auth guard as a global provider on user http module

--- server/src/modules/user/module/http.module.ts
+++ server/src/modules/user/module/http.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { UserService } from '../user.service';
 import { UserModule } from './user.module';
 import { UserController } from '../user.controller';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/modules/auth/auth.guard';
 
 @Module({
   imports: [UserModule],
-  providers: [UserService],
+  providers: [
+    UserService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [UserController],
   exports: [UserService],
 })
